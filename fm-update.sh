c1b1a0ba21ede03e3aea4ec5fdf5368e274d3fc2
Aaron Faanes <dafrito@gmail.com>
2010-05-06 22:09:54 -0500

    update now optionally updates only one component

--- fm-update.sh
+++ fm-update.sh
@@ -4,6 +4,11 @@ PATH=/bin:/usr/bin:$HOME/bin:${0%/*}
 source fm-library.sh
 source fm-load-settings.sh >/dev/null
 
+if [ $1 ]; then
+	save_toc $1 $2
+	exit
+fi
+
 [ $PROJECT ] || error "No project defined"
 
 for comp in `find * -maxdepth 0 -name $PROJECT"_*" -type d | egrep "_.*$" -o | trim 2`
