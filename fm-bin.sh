23e9d7286b6382600045245d538313b1999d3fa8
Aaron Faanes <dafrito@gmail.com>
2010-05-08 05:21:59 -0500

    Lots of work in fm-bin
    * actually uses correct path
    * is safe when used without projects
    * passes FM_ROOT to scripts
    * calls lua files from root dir, rahter than the pwd

--- fm-bin.sh
+++ fm-bin.sh
@@ -1,20 +1,24 @@
 #!/bin/bash
+PATH=/bin:/usr/bin:$HOME/bin:${0%/*}
+FM_ROOT=${0%/*}
 
+source fm-library.sh
 source fm-load-settings.sh >/dev/null
 
-command=${PROJECT-'.'}/bin/$1
-
-if [ -e $command ]; then
-	$command
-	exit $?
-fi;
-if [ -e $command.sh ]; then
-	bash $command.sh
-	exit $?
-fi;
-if [ -e $command.lua ]; then
-	lua $command.lua
-	exit $?
+if [ -d $root/$PROJECT ]; then
+	command="$root/$PROJECT/bin/$1"
+else
+	command="$root/bin/$1"
 fi
-
-exit 1
+shift
+if [ -e $command ]; then
+	FM_ROOT=$FM_ROOT $command $*
+elif [ -e $command.sh ]; then
+	FM_ROOT=$FM_ROOT bash $command.sh $*
+elif [ -e $command.lua ]; then
+	pushd $root >/dev/null
+	lua ${PROJECT+$PROJECT/}bin/$command.lua $*
+	popd >/dev/null
+else
+	echo "Command not found: $command" 1>&2
+fi;
