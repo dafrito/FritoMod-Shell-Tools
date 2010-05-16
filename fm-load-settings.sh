31698777dd6a61cb529624da922e3896d5f30bd3
Aaron Faanes <dafrito@gmail.com>
2010-05-15 22:52:28 -0500

    load-settings now defers to existing PROJECT; make_project now a function

--- fm-load-settings.sh
+++ fm-load-settings.sh
@@ -6,15 +6,19 @@ source fm-library.sh
 make_root
 make_path_using_pwd
 
-if [ "$1" ]; then
-	PROJECT=$1
-elif [ "$path_from_root" ]; then
-	PROJECT=${path_from_root%_*}
-elif [ 1 = `find $root/.fm/* -maxdepth 0 -type d  | wc -l` ]; then
-	PROJECT=`find $root/.fm/* -maxdepth 0 -type d`
-elif [ -e $root/.fm/default ]; then
-	PROJECT=`cat $root/.fm/default`
-fi
+function make_project {
+	if [ $PROJECT ]; then
+		return
+	fi
+	if [ "$path_from_root" ]; then
+		PROJECT=${path_from_root%_*}
+	elif [ 1 = `find $root/.fm/* -maxdepth 0 -type d  | wc -l` ]; then
+		PROJECT=`find $root/.fm/* -maxdepth 0 -type d`
+	elif [ -e $root/.fm/default ]; then
+		PROJECT=`cat $root/.fm/default`
+	fi
+}
+make_project
 
 INTERFACE=${INTERFACE='30300'}
 AUTHOR=${AUTHOR-$USER}
