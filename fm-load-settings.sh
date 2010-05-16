c7a93de210419186d8e0ca2e1e223509c984d11d
Aaron Faanes <dafrito@gmail.com>
2010-05-16 06:50:04 -0500

    load-settings now prefers default over path_from_root

--- fm-load-settings.sh
+++ fm-load-settings.sh
@@ -10,17 +10,18 @@ function make_project {
 	if [ $PROJECT ]; then
 		return
 	fi
-	if [ "$path_from_root" ]; then
-		PROJECT=${path_from_root%_*}
-	elif [ "$root" ] && [ -e $root/.fm ]; then
-		if [ 1 = `find $root/.fm/* -maxdepth 0 -type d  | wc -l` ]; then
-			PROJECT=`find $root/.fm/* -maxdepth 0 -type d`
-		elif [ -e $root/.fm/default ]; then
+	if [ "$root" ] && [ -e $root/.fm ]; then
+		if [ -e $root/.fm/default ]; then
 			PROJECT=`cat $root/.fm/default`
+		elif [ "$path_from_root" ]; then
+			PROJECT=${path_from_root%_*}
+		elif [ 1 = `find $root/.fm/* -maxdepth 0 -type d  | wc -l` ]; then
+			PROJECT=`find $root/.fm/* -maxdepth 0 -type d`
 		fi
 	fi;
 }
 make_project
+
 
 INTERFACE=${INTERFACE='30300'}
 AUTHOR=${AUTHOR-$USER}
