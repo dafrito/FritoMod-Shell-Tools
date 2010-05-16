8b42e5c673775497e73ab20812789f0fbb99135e
Aaron Faanes <dafrito@gmail.com>
2010-05-15 21:29:41 -0500

    fm-load-settings now expects a project argument

--- fm-load-settings.sh
+++ fm-load-settings.sh
@@ -4,17 +4,14 @@ PATH=/bin:/usr/bin:$HOME/bin:$FM_ROOT:${0%/*}
 source fm-library.sh
 
 make_root
-if [ $root ] && [ -d $root ]; then
-	PROJECT=`cat $root/.current-project`
-fi
 
 INTERFACE=${INTERFACE='30300'}
-PROJECT=${PROJECT-''}
+PROJECT=${1-''}
 AUTHOR=${AUTHOR-$USER}
 VERSION=${VERSION-"Unreleased"}
 
-if [ -e $PROJECT/settings ]; then
-	SETTINGS=$PROJECT/settings
+if [ -e $root/$PROJECT/settings ]; then
+	SETTINGS=$root/$PROJECT/settings
 	source $SETTINGS
 fi
 
