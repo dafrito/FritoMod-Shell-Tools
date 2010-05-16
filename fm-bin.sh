0efb9ec9d2d7034c93f94bdd52839acc3b3f6501
Aaron Faanes <dafrito@gmail.com>
2010-05-15 21:28:26 -0500

    bin now only has one bin folder

--- fm-bin.sh
+++ fm-bin.sh
@@ -5,11 +5,8 @@ FM_ROOT=${0%/*}
 source fm-library.sh
 source fm-load-settings.sh >/dev/null
 
-if [ -d $root/$PROJECT ]; then
-	command="$root/$PROJECT/bin/$1"
-else
-	command="$root/bin/$1"
-fi
+command="$root/bin/$1"
+
 shift
 if [ -e $command ]; then
 	FM_ROOT=$FM_ROOT $command $*
@@ -17,7 +14,7 @@ elif [ -e $command.sh ]; then
 	FM_ROOT=$FM_ROOT bash $command.sh $*
 elif [ -e $command.lua ]; then
 	pushd $root >/dev/null
-	lua ${PROJECT+$PROJECT/}bin/$command.lua $*
+	lua bin/$command.lua $*
 	popd >/dev/null
 else
 	echo "Command not found: $command" 1>&2
