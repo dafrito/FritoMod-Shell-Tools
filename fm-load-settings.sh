242962c35875648cc322a6a9068347e374e0a525
Aaron Faanes <dafrito@gmail.com>
2010-05-15 22:07:41 -0500

    load-settings now tries a lot harder to find a project

--- fm-load-settings.sh
+++ fm-load-settings.sh
@@ -6,10 +6,14 @@ source fm-library.sh
 make_root
 make_path_using_pwd
 
-if [ "$path_from_root" ]; then
-	PROJECT=${path_from_root%_*}
-elif [ "$1" ]; then
+if [ "$1" ]; then
 	PROJECT=$1
+elif [ "$path_from_root" ]; then
+	PROJECT=${path_from_root%_*}
+elif [ 1 = `find $root/.fm/* -maxdepth 0 -type d  wc -l` ]; then
+	PROJECT=`find $root/.fm/* -maxdepth 0 -type d`
+elif [ -e $root/.fm/default ]; then
+	PROJECT=`cat $root/.fm/default`
 fi
 
 INTERFACE=${INTERFACE='30300'}
